67b4791be6e5c626303f7d05a7dd2fdcc5aef7a7
Emily Coco <ec3307@nyu.edu>
2019-12-11 10:42:47 -0500

    adding ICRATES data to repository

---
+++ Scripts/ICRATES-time-average.R
@@ -0,0 +1,9 @@
+catalogue = read.csv("Data/ICRATES/ICRATES_CATALOGUE.csv")
+deposit = read.csv("Data/ICRATES/ICRATES_DEPOSIT.csv")
+lrp = read.csv("Data/ICRATES/ICRATES_LRP.csv")
+ock = read.csv("Data/ICRATES/ICRATES_OCK.csv")
+sf = read.csv("Data/ICRATES/ICRATES_STANDARD_FORM.csv")
+
+#need to decide how to build the network and the time-slices
+#network based on shared pottery types
+#time-slices also based on pottery types?
