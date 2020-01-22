e4383ce2dc0ca6a5266439a9262926b6528d14be
Emily Coco <ec3307@nyu.edu>
2020-01-22 15:09:35 -0500

    methods for creating time-averaged networks

--- Scripts/Prignano-time-average.R
+++ Scripts/Prignano-time-average.R
@@ -1,4 +1,5 @@
 library(igraph)
+library(tidyverse)
 
 #read in data and clean
 aa = read.csv("Data/Prignano/AA.gdf")
@@ -7,6 +8,7 @@ colnames(aa.edge) = c("node1", "node2", "weight")
 aa.node = aa[-c(180:nrow(aa)),c(1,3:4)]
 colnames(aa.node) = c("name", "lat", "long")
 aa.node$age = "aa"
+aa.edge$age = "aa"
 
 eia1e = read.csv("Data/Prignano/EIA1E.gdf")
 eia1e.edge = eia1e[118:nrow(eia1e),1:3]
@@ -14,6 +16,7 @@ colnames(eia1e.edge) = c("node1", "node2", "weight")
 eia1e.node = eia1e[-c(117:nrow(eia1e)),c(1,3:4)]
 colnames(eia1e.node) = c("name", "lat", "long")
 eia1e.node$age = "eia1e"
+eia1e.edge$age = "eia1e"
 
 eia1l = read.csv("Data/Prignano/EIA1L.gdf")
 eia1l.edge = eia1l[117:nrow(eia1l),1:3]
@@ -21,6 +24,7 @@ colnames(eia1l.edge) = c("node1", "node2", "weight")
 eia1l.node = eia1l[-c(116:nrow(eia1l)),c(1,3:4)]
 colnames(eia1l.node) = c("name", "lat", "long")
 eia1l.node$age = "eia1l"
+eia1l.edge$age = "eia1l"
 
 eia2 = read.csv("Data/Prignano/EIA2.gdf")
 eia2.edge = eia2[132:nrow(eia2),1:3]
@@ -28,6 +32,7 @@ colnames(eia2.edge) = c("node1", "node2", "weight")
 eia2.node = eia2[-c(131:nrow(eia2)),c(1,3:4)]
 colnames(eia2.node) = c("name", "lat", "long")
 eia2.node$age = "eia2"
+eia2.edge$age = "eia2"
 
 oa = read.csv("Data/Prignano/OA.gdf")
 oa.edge = oa[170:nrow(oa),1:3]
@@ -35,10 +40,66 @@ colnames(oa.edge) = c("node1", "node2", "weight")
 oa.node = oa[-c(169:nrow(oa)),c(1,3:4)]
 colnames(oa.node) = c("name", "lat", "long")
 oa.node$age = "oa"
+oa.edge$age = "oa"
 
 #list of all nodes
 all.nodes = rbind(aa.node, eia1e.node, eia1l.node, eia2.node, oa.node)
 groups = all.nodes %>% group_by(lat, long) %>% 
   mutate(group = group_indices()) #group variable tells us if two nodes are the same
 
-#time averaging functions
+create_new_edge_list = function(edges, groups) {
+  edges$node1 = as.character(edges$node1)
+  edges$node2 = as.character(edges$node2)
+  g1 = groups %>% filter(age == edges$age[1])
+  g1$name = as.character(g1$name)
+  
+  edges$node1.n = edges$node1
+  for(i in 1:nrow(edges)) {
+    edges$node1.n[i] = g1$group[g1$name == trimws(edges$node1[i])]
+  }
+  edges$node2.n = edges$node2
+  for(i in 1:nrow(edges)) {
+    edges$node2.n[i] = g1$group[g1$name == trimws(edges$node2[i])]
+  }
+  return(edges)
+}
+
+#time averaging functions to create combined graphs
+average_two = function(e1, e2, groups) {
+  edge1 = create_new_edge_list(e1, groups)
+  edge2 = create_new_edge_list(e2, groups)
+  edges = rbind(edge1, edge2)
+  graph = simplify(graph_from_edgelist(as.matrix(edges[,5:6])))
+  return(graph)
+}
+
+average_three = function(e1, e2, e3, groups) {
+  edge1 = create_new_edge_list(e1, groups)
+  edge2 = create_new_edge_list(e2, groups)
+  edge3 = create_new_edge_list(e3, groups)
+  e = rbint(edge1, edge2, edge3)
+  graph = simplify(graph_from_edgelist(as.matrix(e[,5:6])))
+  return(graph)
+}
+
+average_four = function(e1, e2, e3, e4, groups) {
+  edge1 = create_new_edge_list(e1, groups)
+  edge2 = create_new_edge_list(e2, groups)
+  edge3 = create_new_edge_list(e3, groups)
+  edge4 = create_new_edge_list(e4, groups)
+  e = rbint(edge1, edge2, edge3, edge4)
+  graph = simplify(graph_from_edgelist(as.matrix(e[,5:6])))
+  return(graph)
+}
+
+average_five = function(e1, e2, e3, e4, e5, groups) {
+  edge1 = create_new_edge_list(e1, groups)
+  edge2 = create_new_edge_list(e2, groups)
+  edge3 = create_new_edge_list(e3, groups)
+  edge4 = create_new_edge_list(e4, groups)
+  edge5 = create_new_edge_list(e5, groups)
+  e = rbind(edge1, edge2, edge3, edge4, edge5)
+  graph = simplify(graph_from_edgelist(as.matrix(e[,5:6])))
+  return(graph)
+}
+
