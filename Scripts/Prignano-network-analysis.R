f934ecbd54425e6d568e0fa333b2dee64776a915
Emily Coco <ec3307@nyu.edu>
2020-08-03 14:14:46 -0400

    updates to model error calculations

--- Scripts/Prignano-network-analysis.R
+++ Scripts/Prignano-network-analysis.R
@@ -3,6 +3,7 @@ source("scripts/Prignano-time-average.R")
 theme_set(theme_minimal())
 library(ggthemes)
 library(lme4)
+library(dplyr)
 
 ####Analysis####
 #EIA1E : Early Iron Age 1 Early (950/925 900 BC)
@@ -59,56 +60,57 @@ alldata = rbind(eia1eta, eia1lta, eia2ta, oata, aata)
 alldata$network = factor(alldata$network, levels = c("EIA1E", "EIA1L", 
                                                      "EIA2", "OA", "AA"))
 
-fit1 = lm(mod ~ num.graphs + network, data = alldata)
-summary(fit1)
-fit2 = lm(mod ~ num.graphs, data = alldata)
-fit3 = lmer(mod ~ num.graphs + (1 | network), data = alldata)
-summary(fit3)
-AICtab(fit2, fit3, base = T, weights = T)
-
-talldata = alldata %>% gather(key = "measure", value = "value", c(1:11))
-ggplot(talldata, aes(x = num.graphs, y = value, group = network, color = network)) +
-  geom_smooth(se = F) + facet_wrap(. ~ measure, scales = "free_y")
-
-pca_all = prcomp(alldata[,c(1:6, 8:11)], scale = T)
-pcadf = data.frame(pca_all$x)
-pcadf$network = alldata$network
-hulls = pcadf %>% group_by(network) %>% dplyr::slice(chull(PC1, PC2))
-ggplot(pcadf, aes(x = PC1, y = PC2, fill = network, color = network)) +
-  geom_point() +
-  geom_polygon(data = hulls, alpha = 0.25) +
-  coord_equal()
-summary(pca_all)
-biplot(pca_all)
-
-avg = alldata %>% filter(num.graphs != 1)
-orig = alldata %>% filter(num.graphs == 1)
-
-pca_avg = prcomp(avg[,c(1:6, 8:11)], scale = T)
-avgdf = data.frame(pca_avg$x)
-avgdf$network = avg$network
-hulls = avgdf %>% group_by(network) %>% dplyr::slice(chull(PC1, PC2))
-ggplot(avgdf, aes(x = PC1, y = PC2, fill = network, color = network)) +
-  geom_point() +
-  coord_equal() +
-  geom_polygon(data = hulls, alpha = 0.25)
-
-summary(pca_avg)
-biplot(pca_avg)
-
-orig.coord = as.data.frame(predict(pca_avg, orig[,c(1:6, 8:11)]))
-rownames(orig.coord) = as.character(orig$network)
-
-p.pca = fviz_pca_biplot(pca_avg, repel = T, pointsize = 2, pointshape = 20, col.var = "grey30", col.ind = avg$network, 
-                addEllipses = T, ellipse.type = "confidence", palette = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"))
-p.pca = fviz_add(p.pca, orig.coord[1,], shape = 15, color = "#E69F00") 
-p.pca = fviz_add(p.pca, orig.coord[2,], shape = 15, color = "#56B4E9") 
-p.pca = fviz_add(p.pca, orig.coord[3,], shape = 15, color = "#009E73") 
-p.pca = fviz_add(p.pca, orig.coord[4,], shape = 15, color = "#F0E442") 
-p.pca = fviz_add(p.pca, orig.coord[5,], shape = 15, color = "#0072B2") 
-plot(p.pca)
-
-ggsave("figures/pca/pca-biplot.png", p.pca)
+# fit1 = lm(edge.dens ~ num.graphs + network, data = alldata)
+# summary(fit1)
+# fit2 = lm(edge.dens ~ num.graphs, data = alldata)
+# summary(fit2)
+# fit3 = lmer(edge.dens ~ num.graphs + (1 | network), data = alldata)
+# summary(fit3)
+# AICtab(fit2, fit3, base = T, weights = T)
+# 
+# talldata = alldata %>% gather(key = "measure", value = "value", c(1:11))
+# ggplot(talldata, aes(x = num.graphs, y = value, group = network, color = network)) +
+#   geom_smooth(se = F) + facet_wrap(. ~ measure, scales = "free_y")
+# 
+# pca_all = prcomp(alldata[,c(1:6, 8:11)], scale = T)
+# pcadf = data.frame(pca_all$x)
+# pcadf$network = alldata$network
+# hulls = pcadf %>% group_by(network) %>% dplyr::slice(chull(PC1, PC2))
+# ggplot(pcadf, aes(x = PC1, y = PC2, fill = network, color = network)) +
+#   geom_point() +
+#   geom_polygon(data = hulls, alpha = 0.25) +
+#   coord_equal()
+# summary(pca_all)
+# biplot(pca_all)
+# 
+# avg = alldata %>% filter(num.graphs != 1)
+# orig = alldata %>% filter(num.graphs == 1)
+# 
+# pca_avg = prcomp(avg[,c(1:6, 8:11)], scale = T)
+# avgdf = data.frame(pca_avg$x)
+# avgdf$network = avg$network
+# hulls = avgdf %>% group_by(network) %>% dplyr::slice(chull(PC1, PC2))
+# ggplot(avgdf, aes(x = PC1, y = PC2, fill = network, color = network)) +
+#   geom_point() +
+#   coord_equal() +
+#   geom_polygon(data = hulls, alpha = 0.25)
+# 
+# summary(pca_avg)
+# biplot(pca_avg)
+# 
+# orig.coord = as.data.frame(predict(pca_avg, orig[,c(1:6, 8:11)]))
+# rownames(orig.coord) = as.character(orig$network)
+# 
+# p.pca = fviz_pca_biplot(pca_avg, repel = T, pointsize = 2, pointshape = 20, col.var = "grey30", col.ind = avg$network, 
+#                 addEllipses = T, ellipse.type = "confidence", palette = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"))
+# p.pca = fviz_add(p.pca, orig.coord[1,], shape = 15, color = "#E69F00") 
+# p.pca = fviz_add(p.pca, orig.coord[2,], shape = 15, color = "#56B4E9") 
+# p.pca = fviz_add(p.pca, orig.coord[3,], shape = 15, color = "#009E73") 
+# p.pca = fviz_add(p.pca, orig.coord[4,], shape = 15, color = "#F0E442") 
+# p.pca = fviz_add(p.pca, orig.coord[5,], shape = 15, color = "#0072B2") 
+# plot(p.pca)
+# 
+# ggsave("figures/pca/pca-biplot.png", p.pca)
 
   
 
@@ -122,89 +124,10 @@ get_null_model_values = function(graph, FUN = calc.diam) {
   return(values)
 }
 
-#create graphs
-create_null_model_graph = function(graph, avg.data, orig.name) {
-  p1 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.mean.between)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.001) +
-    geom_vline(data = avg.data, aes(xintercept = btwn, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "betweeness", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  p2 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.mean.eigen)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.01) +
-    geom_vline(data = avg.data, aes(xintercept = eigen, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "eigencentrality", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  p3 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.mean.path.length)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.005) +
-    geom_vline(data = avg.data, aes(xintercept = path.length, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "path length", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  p4 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.diam)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.01) +
-    geom_vline(data = avg.data, aes(xintercept = diam, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "diameter", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  p5 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.cc)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.01) +
-    geom_vline(data = avg.data, aes(xintercept = cc, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "clustering coefficient", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  p6 = ggplot(data.frame(metric = get_null_model_values(graph, FUN = calc.mod)), aes(x = metric)) +
-    geom_histogram(binwidth = 0.01) +
-    geom_vline(data = avg.data, aes(xintercept = mod, color = as.factor(num.graphs))) +
-    scale_color_colorblind() +
-    labs(x = "modularity", color = "Networks Averaged", title = paste0("Original Graph: ", orig.name)) +
-    theme(axis.title = element_text(size = 7), 
-          title = element_text(size = 8), 
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 6))
-  
-  return(cowplot::plot_grid(p1, p2, p3, p4, p5, p6))
-}
-
-eia1e.graph = graph_from_edgelist(as.matrix(create_new_edge_list(eia1e.edge, groups)[,5:6]))
-pall = create_null_model_graph(eia1e.graph, eia1eta, "EIA1E")
-ggsave("figures/null-models/eia1e.png", pall, dpi = 300, width = 8.5, height = 5)
-
-eia1l.graph = graph_from_edgelist(as.matrix(create_new_edge_list(eia1l.edge, groups)[,5:6]))
-pall = create_null_model_graph(eia1l.graph, eia1lta, "EIA1L")
-ggsave("figures/null-models/eia1l.png", pall, dpi = 300, width = 8.5, height = 5)
-
-eia2.graph = graph_from_edgelist(as.matrix(create_new_edge_list(eia2.edge, groups)[,5:6]))
-pall = create_null_model_graph(eia2.graph, eia2ta, "EIA2")
-ggsave("figures/null-models/eia2.png", pall, dpi = 300, width = 8.5, height = 5)
-
-oa.graph = graph_from_edgelist(as.matrix(create_new_edge_list(oa.edge, groups)[,5:6]))
-pall = create_null_model_graph(oa.graph, oata, "OA")
-ggsave("figures/null-models/oa.png", pall, dpi = 300, width = 8.5, height = 5)
-
-aa.graph = graph_from_edgelist(as.matrix(create_new_edge_list(aa.edge, groups)[,5:6]))
-pall = create_null_model_graph(aa.graph, aata, "AA")
-ggsave("figures/null-models/aa.png", pall, dpi = 300, width = 8.5, height = 5)
-
 ####Model Error Comparison####
 #functions = list(calc.mean.between, calc.eigen, calc.mean.path.length, calc.diam, calc.cc, calc.mod)
 
+##need to update the way this is being calculate so it's not dependent on certain values for normalization
 calculate_model_error = function(graph, df) {
   null.btwn = get_null_model_values(graph, FUN = calc.mean.between)
   df$btwn_diff = (mean(null.btwn) - df$btwn)/(quantile(null.btwn, 0.975) - mean(null.btwn))
@@ -236,12 +159,27 @@ modelerrors = rbind(
 )
 
 me = modelerrors %>% gather(key = "modelerror", value = "value", c(15:20))
-me$network = ordered(me$network, levels = c("EIA1E", "EIA1L", "EIA2", "OA", "AA"))
+me$network = factor(me$network, levels = c("EIA1E", "EIA1L", "EIA2", "OA", "AA"))
 meplot = ggplot(me, aes(x = num.graphs, y = value, group = network, color = network)) +
   geom_rect(xmin = -Inf, xmax = Inf, ymin = -1, ymax = 1, alpha = 0.05, color = NA) +
   geom_hline(yintercept = 0) +
   geom_smooth(se = F) +
   facet_wrap(modelerror ~ ., scales = "free_y")
 
-ggsave("figures/null-models/model-error.png", meplot)
-  
+#ggsave("figures/null-models/model-error.png", meplot)
+
+#need to think of a better way to iterate through the various edge lists
+me_eia1eta = rbind(
+  calculate_model_error(average_two(eia1e.edge, eia1l.edge, groups), (eia1eta %>% filter(names == "ta2"))), 
+  calculate_model_error(average_three(eia1e.edge, eia1l.edge, eia2.edge, groups), (eia1eta %>% filter(names == "ta3"))), 
+  calculate_model_error(average_four(eia1e.edge, eia1l.edge, eia2.edge, oa.edge, groups), (eia1eta %>% filter(names == "ta4"))), 
+  calculate_model_error(average_five(eia1e.edge, eia1l.edge, eia2.edge, oa.edge, aa.edge, groups), (eia1eta %>% filter(names == "ta5")))
+)
+
+me_eia1eta = rbind(
+  calculate_model_error(average_two(eia1e.edge, eia1l.edge, groups), eia1lta[2,]),
+  calculate_model_error(average_two(eia1l.edge, eia2.edge, groups), eia1lta[3,]),
+  calculate_model_error(average_three(eia1e.edge, eia1l.edge, eia2.edge, groups), (eia1eta %>% filter(names == "ta3"))), 
+  calculate_model_error(average_four(eia1e.edge, eia1l.edge, eia2.edge, oa.edge, groups), (eia1eta %>% filter(names == "ta4"))), 
+  calculate_model_error(average_five(eia1e.edge, eia1l.edge, eia2.edge, oa.edge, aa.edge, groups), (eia1eta %>% filter(names == "ta5")))
+)
