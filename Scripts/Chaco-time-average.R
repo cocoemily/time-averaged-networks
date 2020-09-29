dc1b79b127ad92fc0f92c29c96f578d76aa926aa
Emily Coco <ec3307@nyu.edu>
2020-09-29 11:47:52 -0400

    checking Chaco data

---
+++ Scripts/Chaco-time-average.R
@@ -0,0 +1,15 @@
+
+chaco = read.csv("Data/Chaco/AllCeramics.csv")
+
+ad800 = read.csv("Data/Chaco/AD800cer.csv")
+ad850 = read.csv("Data/Chaco/AD850cer.csv")
+ad900 = read.csv("Data/Chaco/AD900cer.csv")
+ad950 = read.csv("Data/Chaco/AD950cer.csv")
+ad1000 = read.csv("Data/Chaco/AD1000cer.csv")
+ad1050 = read.csv("Data/Chaco/AD1050cer.csv")
+ad1100 = read.csv("Data/Chaco/AD1100cer.csv")
+ad1150 = read.csv("Data/Chaco/AD1150cer.csv")
+ad1200 = read.csv("Data/Chaco/AD1200cer.csv")
+ad1250 = read.csv("Data/Chaco/AD1250cer.csv")
+
+#need to decide how to connect sites
