2a2b7504f9a179d18ea135c3056d5a8f95fe2bb3
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-06-01 15:29:25 -0700

    Updated the network_metrics to have the igraph:: specification for the various functions, as in some cases these clash with other packages.

--- Scripts/network_metrics.R
+++ Scripts/network_metrics.R
@@ -3,17 +3,17 @@ library(igraph)
 
 #betweenness centrality normalized by number of nodes
 calc.mean.between <- function(g) {
-  btwn <- betweenness(g, normalized = T)
+  btwn <- igraph::betweenness(g, normalized = T)
   return(mean(btwn))
 }
 #eigenvector centrality 
 calc.mean.eigen <- function(g) {
-  eigen <- eigen_centrality(g)
+  eigen <- igraph::eigen_centrality(g)
   return(mean(eigen$vector))
 }
 #Boncich power centrality
 calc.B.power = function(g) {
-  power = power_centrality(g, rescale = T)
+  power = igraph::power_centrality(g, rescale = T)
   return(mean(power))
 }
 
@@ -24,39 +24,39 @@ calc.mean.path.length <- function(g) {
 }
 #diameter normalized by size
 calc.diam <- function(g) {
-  diam <- diameter(g)
+  diam <- igraph::diameter(g)
   return(diam/calc.S(g))
 }
 #edge density
 calc.edge.dens <- function(g) {
-  ed <- edge_density(g)
+  ed <- igraph::edge_density(g)
   return(ed)
 }
 #size
 calc.S <- function(g){
-  S <- vcount(g)
+  S <- igraph::vcount(g)
   return(S)
 }
 #clustering coefficient
 calc.cc <- function(g) {
-  cc <- transitivity(g, "global")
+  cc <- igraph::transitivity(g, "global")
   return(cc)
 }
 #modularity
 calc.mod <- function(g) {
-  wtc = cluster_walktrap(g)
-  mod <- modularity(as.undirected(g), membership(wtc))
+  wtc = igraph::cluster_walktrap(g)
+  mod <- igraph::modularity(as.undirected(g), membership(wtc))
   return(mod)
 }
 #mean degree
 calc.mean.deg = function(g) {
-  return(mean(degree(g, mode = "all", normalized = T)))
+  return(mean(igraph::degree(g, mode = "all", normalized = T)))
 }
 #mean in degree
 calc.mean.in = function(g) {
-  return(mean(degree(g, mode = "in", normalized = T)))
+  return(mean(igraph::degree(g, mode = "in", normalized = T)))
 }
 #mean out degree
 calc.mean.out = function(g) {
-  return(mean(degree(g, mode = "out", normalized = T)))
+  return(mean(igraph::degree(g, mode = "out", normalized = T)))
 }
