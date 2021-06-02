3d28cc1176f4cf986fff281715b4e72a39efa9ba
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2021-06-01 18:44:26 -0700

    Updated the node_metrics to have the igraph:: specification for the various functions, as in some cases these clash with other packages.

--- Scripts/node_metrics.R
+++ Scripts/node_metrics.R
@@ -1,28 +1,28 @@
 library(igraph)
 
 calc.node.in = function(g) {
-  din = as.data.frame(degree(g, mode = "in"))
+  din = as.data.frame(igraph::degree(g, mode = "in"))
   din$node = rownames(din)
   colnames(din) = c("metric", "node")
   return(din)
 }
 
 calc.node.out = function(g) {
-  dout = as.data.frame(degree(g, mode = "out"))
+  dout = as.data.frame(igraph::degree(g, mode = "out"))
   dout$node = rownames(dout)
   colnames(dout) = c("metric", "node")
   return(dout)
 }
 
 calc.node.deg = function(g) {
-  dall = as.data.frame(degree(g, mode = "all"))
+  dall = as.data.frame(igraph::degree(g, mode = "all"))
   dall$node = rownames(dall)
   colnames(dall) = c("metric", "node")
   return(dall)
 }
 
 calc.eigen = function(g) {
-  ec = eigen_centrality(g)$vector
+  ec = igraph::eigen_centrality(g)$vector
   ecdf = as.data.frame(ec)
   ecdf$node = as.numeric(rownames(ecdf))
   colnames(ecdf) = c("metric", "node")
@@ -30,18 +30,18 @@ calc.eigen = function(g) {
 }
 
 calc.btwn = function(g) {
-  bdf = as.data.frame(betweenness(g))
+  bdf = as.data.frame(igraph::betweenness(g))
   bdf$node = as.numeric(rownames(bdf))
   colnames(bdf) = c("metric", "node")
   return(bdf)
 }
 
 # calc.close = function(g) {
-#   return(closeness(g))
+#   return(igraph::closeness(g))
 # }
 
 calc.deg.dist = function(g) {
-  ddist = as.data.frame(degree.distribution(g))
+  ddist = as.data.frame(igraph::degree.distribution(g))
   ddist$degree = as.numeric(rownames(ddist))
   colnames(ddist) = c("freq", "degree")
   return(ddist)
