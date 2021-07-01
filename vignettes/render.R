701e7df33a54b3ef1bd3abc3ca33fa4420788416
cocoemily <ec3307@log-1.nyu.cluster>
2021-07-01 13:50:01 -0400

    Prignano SA with 100 replications

--- vignettes/render.R
+++ vignettes/render.R
@@ -1,10 +1,10 @@
 ##Rscript for knitting RMarkdown files on an HPC
 
 #run this line to render the Prignano sensitivity analysis document
-rmarkdown::render("vignettes/Sensitivity_Analysis_Prignano.Rmd")
+#rmarkdown::render("vignettes/Sensitivity_Analysis_Prignano.Rmd")
 
 #run this line to render the ICRATES sensitivity analysis document
-#rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES.Rmd")
+rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES.Rmd")
 
 #run this line to render the Chaco sensitivity analysis document
 #rmarkdown::render("vignettes/Sensitivity_Analysis.Rmd")
