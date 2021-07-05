09d8870e075ff81f80eaf1c97935d476cfc5caf5
cocoemily <ec3307@log-3.nyu.cluster>
2021-07-05 18:21:34 -0400

    updated Prignano graph objects

--- Scripts/get-all-graphs.R
+++ Scripts/get-all-graphs.R
@@ -17,12 +17,17 @@ rm(list = setdiff(ls(), c("Chaco_original_graphs", "Chaco_ta_graphs",
                           "Prignano_original_graphs", "Prignano_ta_graphs",
                           "ICRATES_original_graphs", "ICRATES_ta_graphs")))
 
-save(Chaco_original_graphs, Chaco_ta_graphs, file = "Data/Chaco_graph_objects.RData")
+save(Chaco_original_graphs, file = "Data/Chaco_original_graph_objects.RData")
+save(Chaco_ta_graphs, file = "Data/Chaco_ta_graph_objects.RData")
 save(Prignano_original_graphs, Prignano_ta_graphs, file = "Data/Prignano_graph_objects.RData")
 save(ICRATES_original_graphs, file = "Data/ICRATES_original_graph_objects.RData")
-first_half = ICRATES_ta_graphs[1:25]
-second_half = ICRATES_ta_graphs[26:50]
-save(first_half, file = "Data/ICRATES_ta_graph_objects_first-half.RData")
-save(second_half, file = "Data/ICRATES_ta_graph_objects_second-half.RData")
+first = ICRATES_ta_graphs[1:12]
+second = ICRATES_ta_graphs[13:25]
+third = ICRATES_ta_graphs[26:38]
+fourth = ICRATES_ta_graphs[39:50]
+save(first, file = "Data/ICRATES_ta_graph_objects_1.RData")
+save(second, file = "Data/ICRATES_ta_graph_objects_2.RData")
+save(third, file = "Data/ICRATES_ta_graph_objects_3.RData")
+save(fourth, file = "Data/ICRATES_ta_graph_objects_4.RData")
 
 
