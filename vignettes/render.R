27f54f4af7f1ba761710771b61b1d2f90194c85a
Emily Coco <ec3307@nyu.edu>
2021-07-12 10:03:45 -0400

    creating a subsample sensitivity analysis for ICRATES dataset

--- vignettes/render.R
+++ vignettes/render.R
@@ -6,5 +6,8 @@
 #run this line to render the ICRATES sensitivity analysis document
 #rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES.Rmd")
 
+#run this line to render the subsample of the ICRATES sensitivity analysis document
+rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES-sample.Rmd")
+
 #run this line to render the Chaco sensitivity analysis document
-rmarkdown::render("vignettes/Sensitivity_Analysis.Rmd")
+#rmarkdown::render("vignettes/Sensitivity_Analysis.Rmd")
