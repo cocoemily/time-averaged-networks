d33cfce70673c121c9b4c27f7c93df753d921104
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-03-20 18:29:10 +0100

    heatmaps presenting sensitivity analysis resuls

---
+++ Scripts/sensitivity-analysis-summary-visualisation-functions.R
@@ -0,0 +1,62 @@
+
+library(reshape2)
+library(highcharter)
+library(RColorBrewer)
+
+# initialize summary results dataframes
+initialize_data = function(dNets, dSF, samp.frac, network.names.full) {
+
+  df_median_dc = df_median_ec = df_median_bc = 
+    df_sd_dc = df_sd_ec = df_sd_bc = 
+    df_IQR_dc = df_IQR_ec = df_IQR_bc = 
+    df_outliers_dc = df_outliers_ec = df_outliers_bc = 
+    as.data.frame(matrix(NA, nrow = dNets, ncol = dSF))
+  colnames(df_median_dc) = colnames(df_median_ec) = colnames(df_median_bc) = 
+    colnames(df_sd_dc) = colnames(df_sd_ec) = colnames(df_sd_bc) = 
+    colnames(df_IQR_dc) = colnames(df_IQR_ec) = colnames(df_IQR_bc) = 
+    colnames(df_outliers_dc) = colnames(df_outliers_ec) = colnames(df_outliers_bc) = samp.frac
+  rownames(df_median_dc) = rownames(df_median_ec) = rownames(df_median_bc) = 
+    rownames(df_sd_dc) = rownames(df_sd_ec) = rownames(df_sd_bc) = 
+    rownames(df_IQR_dc) = rownames(df_IQR_ec) = rownames(df_IQR_bc) = 
+    rownames(df_outliers_dc) = rownames(df_outliers_ec) = rownames(df_outliers_bc) = network.names.full
+  df_cc = df_modularity = as.data.frame(matrix(NA, nrow = dNets, ncol = dSF+1))
+  colnames(df_cc) = colnames(df_modularity) = c("S100", samp.frac)
+  rownames(df_cc) = rownames(df_modularity) = network.names.full
+  
+  return(as.list(environment()))
+  
+}
+
+# summary statistics functions
+fmedian = function(x) {round(median(x, na.rm = TRUE), 3)}
+fsd = function(x) {round(sd(x, na.rm = TRUE), 3)}
+fIQR = function(x) {round(IQR(x, na.rm = TRUE), 3)}
+foutliers = function(x, pIQR) {
+  x = na.omit(x)
+  sum(x < (quantile(x, 0.25) - 1.5*pIQR)) + sum(x > (quantile(x, 0.75) + 1.5*pIQR))
+}
+
+# heatmap plot function
+plot_hc_heatmap = function(melted_df, title, brewerset_Col, countdata) {
+  if(countdata) {
+    colorset = newcol_Cat(length(na.omit(unique(melted_df$value))))
+  } else {
+    colorset = newcol_Con(length(na.omit(unique(melted_df$value))))   
+  } 
+  df_colorVar = cbind.data.frame(sort(na.omit(unique(melted_df$value))), colorset, stringsAsFactors = FALSE)
+  df_colorVar = rbind(df_colorVar, c(NA, "#ffffff"))
+  names(df_colorVar)[1] = "value"
+  names(df_colorVar)[2] = "colvar"
+  melted_df = merge(melted_df, df_colorVar, by = "value")
+  hc = hchart(melted_df, 
+              type = "heatmap", 
+              hcaes(x = Var2, y = Var1, value = value, color = colvar)) %>%
+    hc_xAxis(title = list(text = "Sampling fraction")) %>% 
+    hc_yAxis(title = list(text = "All networks")) %>% 
+    hc_tooltip(pointFormat = paste("Network: {point.y} <br> Spearman's Rho: {point.value}")) %>%
+    hc_colorAxis(minColor = brewerset_Col[1], maxColor = brewerset_Col[length(brewerset_Col)],
+                 stops = color_stops(length(brewerset_Col), brewerset_Col)) %>%
+    hc_title(text = title)
+  return(hc)
+}
+
---
+++ Scripts/sensitivity-analysis-summary-visualisation.R
@@ -0,0 +1,172 @@
+# Visualize Sensitivy Analysis Results in Heatmaps
+source("Scripts/sensitivity-analysis-summary-visualisation-functions.R")
+
+# name of the dataset
+dtName = "Prignano"
+# # OR
+# dtName = "ICRATES"
+# # OR
+# dtName = "Chaco"
+
+
+# where does the sensitivity analysis refer to?
+dtAnalysis = "nodes" # Potential impact of missing nodes
+# # OR
+# dtAnalysis = "edges" # Potential impact of missing edges
+
+# load names of networks
+if (dtName == "Prignano") {
+  network.names.full = c("EIA1E_network_1", "EIA1E_network_2", "EIA1E_network_3", "EIA1E_network_4", 
+                         "EIA1E_network_5", "EIA1L_network_1", "EIA1L_network_2a", "EIA1L_network_2b", 
+                         "EIA1L_network_3a", "EIA1L_network_3b", "EIA1L_network_4a", "EIA1L_network_4b", 
+                         "EIA1L_network_5", "EIA2_network_1", "EIA2_network_2a", "EIA2_network_2b", 
+                         "EIA2_network_3a", "EIA2_network_3b", "EIA2_network_3c", "EIA2_network_4a", 
+                         "EIA2_network_4b", "EIA2_network_5", "OA_network_1", "OA_network_2a", 
+                         "OA_network_2b", "OA_network_3a", "OA_network_3b", "OA_network_4a", 
+                         "OA_network_4b", "OA_network_5", "AA_network_1", "AA_network_2", 
+                         "AA_network_3", "AA_network_4", "AA_network_5")
+  rowheight = 380
+  
+} else if (dtName == "ICRATES") {
+  network.names.full = icrates.net.names.full$period_network # from senstivity analysis
+  rowheight = 1000 # or potentially larger
+  
+} else if (dtName == "Chaco") {
+  network.names.full = NULL
+  rowheight = 380
+}
+
+# load data according to the input above
+SA_results = readRDS(paste0("Data/sensitivity_analysis_", dtAnalysis, "_", dtName, ".rds"))
+
+# define colors
+brewerset_Con = brewer.pal(11, name = "BrBG")[c(1:4, 8:11)]
+newcol_Con = colorRampPalette(brewerset_Con)
+brewerset_Cat = brewer.pal(9, name = "BuPu")
+newcol_Cat = colorRampPalette(brewerset_Cat)
+
+# number of networks
+dNets = length(SA_results)
+
+# sampling fraction
+samp.frac = c("S90", "S80", "S70", "S60", "S50", "S40", "S30", "S20", "S10")
+dSF = length(samp.frac)
+
+# initialize results data
+list_res = initialize_data(dNets = dNets, dSF = dSF, samp.frac = samp.frac, network.names.full = network.names.full)
+
+# loop over all networks
+for (i in 1:dNets) {
+  
+  # data per network
+  SA.rs.i = SA_results[[i]]
+  SA.rs.i.v1 = as.data.frame(SA.rs.i[[1]])
+  SA.rs.i.v2 = as.data.frame(SA.rs.i[[2]])
+  SA.rs.i.v3 = as.data.frame(SA.rs.i[[3]])
+  SA.rs.i.v4 = as.data.frame(SA.rs.i[[4]])
+  SA.rs.i.v5 = as.data.frame(SA.rs.i[[5]])
+  
+  # initialize outliers data 1D vector
+  voutliers.1 = rep(NA, dSF)
+  voutliers.2 = rep(NA, dSF)
+  voutliers.3 = rep(NA, dSF)
+  
+  # get summary results: median
+  vmedian.1 = apply(SA.rs.i.v1, 2, fmedian)
+  vmedian.2 = apply(SA.rs.i.v2, 2, fmedian)
+  vmedian.3 = apply(SA.rs.i.v3, 2, fmedian)
+  
+  # get summary results: standard deviation
+  vsd.1 = apply(SA.rs.i.v1, 2, fsd)
+  vsd.2 = apply(SA.rs.i.v2, 2, fsd)
+  vsd.3 = apply(SA.rs.i.v3, 2, fsd)
+  
+  # get summary results: interquartile range
+  vIQR.1 = apply(SA.rs.i.v1, 2, fIQR)
+  vIQR.2 = apply(SA.rs.i.v2, 2, fIQR)
+  vIQR.3 = apply(SA.rs.i.v3, 2, fIQR)
+  
+  # get summary results: number of outliers
+  for(j in 1:dSF) {
+    voutliers.1[j] = foutliers(SA.rs.i.v1[,j], vIQR.1[j])
+    voutliers.2[j] = foutliers(SA.rs.i.v2[,j], vIQR.2[j])
+    voutliers.3[j] = foutliers(SA.rs.i.v3[,j], vIQR.3[j])
+  }
+  
+  # update data in results list
+  list_res$df_median_dc[i,] = vmedian.1
+  list_res$df_sd_dc[i,] = vsd.1
+  list_res$df_IQR_dc[i,] = vIQR.1
+  list_res$df_outliers_dc[i,] = voutliers.1
+  
+  list_res$df_median_ec[i,] = vmedian.2
+  list_res$df_sd_ec[i,] = vsd.2
+  list_res$df_IQR_ec[i,] = vIQR.2
+  list_res$df_outliers_ec[i,] = voutliers.2
+  
+  list_res$df_median_bc[i,] = vmedian.3
+  list_res$df_sd_bc[i,] = vsd.3
+  list_res$df_IQR_bc[i,] = vIQR.3
+  list_res$df_outliers_bc[i,] = voutliers.3
+  
+  list_res$df_cc[i,] = SA.rs.i.v4
+  list_res$df_modularity[i,] = SA.rs.i.v5
+}
+
+# melt data
+# degree cetrality
+melted_median_dc = melt(as.matrix(list_res$df_median_dc)); melted_median_dc$value = round(melted_median_dc$value, 2)
+melted_sd_dc = melt(as.matrix(list_res$df_sd_dc)); melted_sd_dc$value = round(melted_sd_dc$value, 2)
+melted_IQR_dc = melt(as.matrix(list_res$df_IQR_dc)); melted_IQR_dc$value = round(melted_IQR_dc$value, 2)
+melted_outliers_dc = melt(as.matrix(list_res$df_outliers_dc)); melted_outliers_dc$value = round(melted_outliers_dc$value, 2)
+# eigenvector centrality
+melted_median_ec = melt(as.matrix(list_res$df_median_ec)); melted_median_ec$value = round(melted_median_ec$value, 2)
+melted_sd_ec = melt(as.matrix(list_res$df_sd_ec)); melted_sd_ec$value = round(melted_sd_ec$value, 2)
+melted_IQR_ec = melt(as.matrix(list_res$df_IQR_ec)); melted_IQR_ec$value = round(melted_IQR_ec$value, 2)
+melted_outliers_ec = melt(as.matrix(list_res$df_outliers_ec)); melted_outliers_ec$value = round(melted_outliers_ec$value, 2)
+# betweeness centrality
+melted_median_bc = melt(as.matrix(list_res$df_median_bc)); melted_median_bc$value = round(melted_median_bc$value, 2)
+melted_sd_bc = melt(as.matrix(list_res$df_sd_bc)); melted_sd_bc$value = round(melted_sd_bc$value, 2)
+melted_IQR_bc = melt(as.matrix(list_res$df_IQR_bc)); melted_IQR_bc$value = round(melted_IQR_bc$value, 2)
+melted_outliers_bc = melt(as.matrix(list_res$df_outliers_bc)); melted_outliers_bc$value = round(melted_outliers_bc$value, 2)
+# clustering coefficient
+melted_cc = melt(as.matrix(list_res$df_cc)); melted_cc$value = round(melted_cc$value, 2)
+# modularity
+melted_mo = melt(as.matrix(list_res$df_mo)); melted_mo$value = round(melted_mo$value, 2)
+
+# plots
+# degree centrality
+hc.dc.a = plot_hc_heatmap(melted_df = melted_median_dc, title = paste0("Median Degree Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.dc.b = plot_hc_heatmap(melted_df = melted_sd_dc, title = paste0("SD Degree Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.dc.c = plot_hc_heatmap(melted_df = melted_IQR_dc, title = paste0("IQR Degree Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.dc.d = plot_hc_heatmap(melted_df = melted_outliers_dc, title = paste0("Outliers Degree Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Cat, countdata = TRUE)
+# eigenvector centrality
+hc.ec.a = plot_hc_heatmap(melted_df = melted_median_ec, title = paste0("Median Eigenvector Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.ec.b = plot_hc_heatmap(melted_df = melted_sd_ec, title = paste0("SD Eigenvector Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.ec.c = plot_hc_heatmap(melted_df = melted_IQR_ec, title = paste0("IQR Eigenvector Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.ec.d = plot_hc_heatmap(melted_df = melted_outliers_ec, title = paste0("Outliers Eigenvector Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Cat, countdata = TRUE)
+# betweeness centrality
+hc.bc.a = plot_hc_heatmap(melted_df = melted_median_bc, title = paste0("Median Betweeness Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.bc.b = plot_hc_heatmap(melted_df = melted_sd_bc, title = paste0("SD Betweeness Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.bc.c = plot_hc_heatmap(melted_df = melted_IQR_bc, title = paste0("IQR Betweeness Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+hc.bc.d = plot_hc_heatmap(melted_df = melted_outliers_bc, title = paste0("Outliers Betweeness Centrality (", dtAnalysis, ")"), brewerset_Col = brewerset_Cat, countdata = TRUE)
+# clustering coefficient
+hc.cc = plot_hc_heatmap(melted_df = melted_cc, title = paste0("Clustering Coefficient (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+# modularity
+hc.mo = plot_hc_heatmap(melted_df = melted_mo, title = paste0("Modularity (", dtAnalysis, ")"), brewerset_Col = brewerset_Con, countdata = FALSE)
+  
+# arrange plots in one page
+charts.dc = list(hc.dc.a, hc.dc.b, hc.dc.c, hc.dc.d)
+hc.dc.grid = hw_grid(charts.dc, ncol = 2, rowheight = rowheight)
+charts.ec = list(hc.ec.a, hc.ec.b, hc.ec.c, hc.ec.d)
+hc.ec.grid = hw_grid(charts.ec, ncol = 2, rowheight = rowheight)
+charts.bc = list(hc.bc.a, hc.bc.b, hc.bc.c, hc.bc.d)
+hc.bc.grid = hw_grid(charts.bc, ncol = 2, rowheight = rowheight)
+
+# save plots in html file
+htmltools::save_html(hc.dc.grid, paste0("viz_summary_DC_", dtAnalysis, "_", dtName, ".html"))
+htmltools::save_html(hc.ec.grid, paste0("viz_summary_EC_", dtAnalysis, "_", dtName, ".html"))
+htmltools::save_html(hc.bc.grid, paste0("viz_summary_BC_", dtAnalysis, "_", dtName, ".html"))
+htmltools::save_html(hc.cc, paste0("viz_summary_CC_", dtAnalysis, "_", dtName, ".html"))
+htmltools::save_html(hc.mo, paste0("viz_summary_MO_", dtAnalysis, "_", dtName, ".html"))
+
