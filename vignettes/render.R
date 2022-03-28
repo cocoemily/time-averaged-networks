c72e7f150c938785b0769c81e2434ec719489e9c
cocoemily <ec3307@nyu.edu>
2022-03-28 10:21:22 -0400

    moving data read in to render script to determine source of error

--- vignettes/render.R
+++ vignettes/render.R
@@ -7,7 +7,10 @@
 #rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES.Rmd")
 
 #run this line to render the subsample of the ICRATES sensitivity analysis document
-rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES-sample.Rmd")
+#rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES-sample.Rmd")
 
 #run this line to render the Chaco sensitivity analysis document
-#rmarkdown::render("vignettes/Sensitivity_Analysis.Rmd")
+load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_1.RData")
+load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_2.RData")
+load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_3.RData")
+rmarkdown::render("vignettes/Sensitivity_Analysis.Rmd")
