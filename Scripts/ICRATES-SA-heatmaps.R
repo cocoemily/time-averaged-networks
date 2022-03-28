c40a3ba1084848f87adcbbf0efa4220d4a8acce1
cocoemily <ec3307@nyu.edu>
2022-03-28 10:11:45 -0400

    hardcoding path name for heatmap creation

--- Scripts/ICRATES-SA-heatmaps.R
+++ Scripts/ICRATES-SA-heatmaps.R
@@ -7,7 +7,7 @@ dtName = "ICRATES"
 rowheight = 1000 # or potentially larger, please change as you see fit.
 
 # names of networks
-load("../Data/icrates_network_names.RData")
+load("/scratch/ec3307/time-averaged-networks/Data/icrates_network_names.RData")
 network.names.full = icrates.net.names.full$period_network
 
 # Get output visuals - potential impact of missing nodes
