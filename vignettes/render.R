3557e300699ca28bfc705787ff5d8ba4a1dc4109
cocoemily <ec3307@nyu.edu>
2022-03-28 10:32:48 -0400

    fixing relative path names for HPC runs

--- vignettes/render.R
+++ vignettes/render.R
@@ -1,16 +1,13 @@
 ##Rscript for knitting RMarkdown files on an HPC
 
 #run this line to render the Prignano sensitivity analysis document
-#rmarkdown::render("vignettes/Sensitivity_Analysis_Prignano.Rmd")
+#rmarkdown::render("../vignettes/Sensitivity_Analysis_Prignano.Rmd")
 
 #run this line to render the ICRATES sensitivity analysis document
-#rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES.Rmd")
+#rmarkdown::render("../vignettes/Sensitivity_Analysis_ICRATES.Rmd")
 
 #run this line to render the subsample of the ICRATES sensitivity analysis document
-#rmarkdown::render("vignettes/Sensitivity_Analysis_ICRATES-sample.Rmd")
+#rmarkdown::render("../vignettes/Sensitivity_Analysis_ICRATES-sample.Rmd")
 
 #run this line to render the Chaco sensitivity analysis document
-# load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_1.RData")
-# load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_2.RData")
-# load("/scratch/ec3307/time-averaged-networks/Data/Chaco_ta_graph_objects_3.RData")
 rmarkdown::render("../vignettes/Sensitivity_Analysis.Rmd")
