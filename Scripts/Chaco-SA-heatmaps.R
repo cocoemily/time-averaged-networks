30edde023eeca70be9d08a8373c43e42cd8833db
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-04-25 22:36:23 +0200

    Renamed file

--- Scripts/Chaco-SA-heatmaps.R
+++ Scripts/Chaco-SA-heatmaps.R
@@ -12,10 +12,10 @@ network.names.full = chaco.net.names.full$period_network
 
 # Get output visuals - potential impact of missing nodes
 dtAnalysis = "nodes" 
-source("Scripts/sensitivity-analysis-summary-visualisation.R")
+source("Scripts/sensitivity-analysis-summary-visualisation-heatmap.R")
 
 # Get output visuals - potential impact of missing edges
 dtAnalysis = "edges"
-source("Scripts/sensitivity-analysis-summary-visualisation.R")
+source("Scripts/sensitivity-analysis-summary-visualisation-heatmap.R")
 
 
