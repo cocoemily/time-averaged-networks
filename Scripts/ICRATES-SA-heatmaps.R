66ffb9a706ed51a9779b462a1aff448756ebe985
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-04-25 22:37:09 +0200

    Renamed file

--- Scripts/ICRATES-SA-heatmaps.R
+++ Scripts/ICRATES-SA-heatmaps.R
@@ -12,9 +12,9 @@ network.names.full = icrates.net.names.full$period_network
 
 # Get output visuals - potential impact of missing nodes
 dtAnalysis = "nodes" 
-source("Scripts/sensitivity-analysis-summary-visualisation.R")
+source("Scripts/sensitivity-analysis-summary-visualisation-heatmap.R")
 
 # Get output visuals - potential impact of missing edges
 dtAnalysis = "edges"
-source("Scripts/sensitivity-analysis-summary-visualisation.R")
+source("Scripts/sensitivity-analysis-summary-visualisation-heatmap.R")
 
