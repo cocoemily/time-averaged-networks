03f01860ec82cf85562df5bb15db1c68f1f5906c
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-04-26 19:59:38 +0200

    added specific plot attributes

--- Scripts/Prignano-SA-pointplots.R
+++ Scripts/Prignano-SA-pointplots.R
@@ -3,6 +3,10 @@
 # name of dataset
 dtName = "Prignano"
 
+# data specific plot attributes
+pointsize = 2 # size of points
+alphavalue = 0.7 # opacity of points
+
 # Get output visuals - potential impact of missing nodes
 dtAnalysis = "nodes"
 source("Scripts/sensitivity-analysis-summary-visualisation-points.R")
