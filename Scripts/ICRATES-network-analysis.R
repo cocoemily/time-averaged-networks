5c17420232d9590eb4bef35ce9abbac4d34666eb
Danai Kafetzaki <danae.kafetzaki@gmail.com>
2022-06-24 14:22:09 +0200

    updated pca biplot

--- Scripts/ICRATES-network-analysis.R
+++ Scripts/ICRATES-network-analysis.R
@@ -97,7 +97,7 @@ for (i in 1:length(plot.nets.all)) {
   ggsave(paste0("figures/pca/ICRATES/pca-biplot_", i,"_.pdf"), pcaplot_i)
 }
 
-write.csv(get_pca_variable_contribs(i_alldata, c("btwn", "eigen", "mean.deg", "cc", "mod", "path.length", "num.graphs")), file = "output/ICRATES/pca-dim-contribs.csv")
+write.csv(get_pca_variable_contribs(alldata_nodu, c("btwn", "eigen", "mean.deg", "cc", "mod", "path.length", "num.graphs")), file = "output/ICRATES/pca-dim-contribs.csv")
 
 
 ####Model Errors####
